754d76bae2cc862efadc91a3e99f291c4af66d28
Marcio Vinicius de Melo de Alvarenga <164529046+MarcioAlvarenga-AlfaDaemon@users.noreply.github.com>
2026-05-10 16:07:16 -0300

    s

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-//Percorrer (iterar) uma lista de elementos
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
@@ -11,7 +10,7 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
-// document.querySelector("#minhaDiv p").textContent = "Chega de calor";
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-04-03T00:00:00");
 const tempoObjetivo2 = new Date("2024-10-17T00:00:00");
