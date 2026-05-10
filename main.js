64161f7e9729e4cdbc017853155b9a640466924c
Marcio Vinicius de Melo de Alvarenga <164529046+MarcioAlvarenga-AlfaDaemon@users.noreply.github.com>
2026-05-10 15:56:48 -0300

    ssalvar

--- main.js
+++ main.js
@@ -38,15 +38,19 @@ function calculaTempo(tempoObjetivo) {
   horas %= 24;
 
   if (tempoFinal > 0) {
-    return ( dias + " dias " +  horas + " horas " + minutos + " minutos " + segundos +  " segundos");
+    return [dias,horas,minutos,segudos];
   } else {
-    return "Prazo finalizado.";
+    return [0,0,0,0];
   }
 }
 
 function atualizaCronometro() {
+  document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
+  document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
+  document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
+  document.getElementById("seg").textContent = calculaTempo(tempos[1])[3];
   for (let i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
+    //contadores[i].textContent = calculaTempo(tempos[i]);
   }
 }
 
